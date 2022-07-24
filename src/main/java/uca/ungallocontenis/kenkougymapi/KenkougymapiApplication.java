9d22d863409b646a9eed0f5ab021a3b4e358541c
Slackoth <00082318@uca.edu.sv>
2022-07-24 13:35:54 -0500

    Spring Boot Init

---
+++ src/main/java/uca/ungallocontenis/kenkougymapi/KenkougymapiApplication.java
@@ -0,0 +1,13 @@
+package uca.ungallocontenis.kenkougymapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenkougymapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenkougymapiApplication.class, args);
+	}
+
+}

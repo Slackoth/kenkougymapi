6a6a04942c5790ed265635262dc3e9a6f3c03ec7
Slackoth <00082318@uca.edu.sv>
2022-08-11 23:38:39 -0500

    CommandLineRunner for testing added

--- src/main/java/uca/ungallocontenis/kenkougymapi/KenkougymapiApplication.java
+++ src/main/java/uca/ungallocontenis/kenkougymapi/KenkougymapiApplication.java
@@ -1,13 +1,23 @@
 package uca.ungallocontenis.kenkougymapi;
 
+import org.slf4j.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class KenkougymapiApplication {
+public class KenkougymapiApplication implements CommandLineRunner {
+	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(KenkougymapiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(KenkougymapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// For testing...
+		LOG.info("Un Gallo Con Tenis");
 	}
 
 }

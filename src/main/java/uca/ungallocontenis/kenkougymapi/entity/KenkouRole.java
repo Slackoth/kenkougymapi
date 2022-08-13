9f515b5acf74cb59aa06c1d0034fd6ac1872f1cd
Slackoth <00082318@uca.edu.sv>
2022-08-13 12:06:35 -0500

    Authentication and authorization entities added

---
+++ src/main/java/uca/ungallocontenis/kenkougymapi/entity/KenkouRole.java
@@ -0,0 +1,46 @@
+package uca.ungallocontenis.kenkougymapi.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity(name = "KenkouRole")
+@Table(name = "kenkou_role")
+public class KenkouRole {
+    
+    @Id
+    @Column(name = "role_code")
+    private String roleCode;
+    @Column(name = "role_name")
+    private String roleName;
+    @ManyToMany(mappedBy = "usersRoles")
+    private List<KenkouUser> usersByRole;
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public List<KenkouUser> getUsersByRole() {
+        return usersByRole;
+    }
+
+    public void setUsersByRole(List<KenkouUser> usersByRole) {
+        this.usersByRole = usersByRole;
+    }
+}
---
+++ src/main/java/uca/ungallocontenis/kenkougymapi/entity/KenkouUser.java
@@ -0,0 +1,76 @@
+package uca.ungallocontenis.kenkougymapi.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity(name = "KenkouUser")
+@Table(name = "kenkou_user")
+public class KenkouUser {
+    
+    @Id
+    private String username;
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastname;
+    private String password;
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = {@JoinColumn(name = "username", referencedColumnName = "username")}, inverseJoinColumns = {@JoinColumn(name = "role_code", referencedColumnName = "role_code")})
+    private List<KenkouRole> usersRoles;
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<KenkouRole> getUsersRoles() {
+        return usersRoles;
+    }
+
+    public void setUsersRoles(List<KenkouRole> usersRoles) {
+        this.usersRoles = usersRoles;
+    }
+}

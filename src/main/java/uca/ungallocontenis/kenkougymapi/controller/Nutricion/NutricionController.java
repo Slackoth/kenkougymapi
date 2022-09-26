cf1b8062ba214f3d9160f468ed6ec61a9c433927
Slackoth <00082318@uca.edu.sv>
2022-09-26 18:25:07 -0500

    Controller de nutricion agregado con sus respectivos repositorios

--- src/main/java/uca/ungallocontenis/kenkougymapi/controller/Nutricion/NutricionController.java
+++ src/main/java/uca/ungallocontenis/kenkougymapi/controller/Nutricion/NutricionController.java
@@ -1,9 +1,30 @@
 package uca.ungallocontenis.kenkougymapi.controller.Nutricion;
 
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uca.ungallocontenis.kenkougymapi.repository.Nutricion.EstiloAlimenticioRepository;
+import uca.ungallocontenis.kenkougymapi.repository.Nutricion.IngredienteRepository;
+import uca.ungallocontenis.kenkougymapi.repository.Nutricion.PlanAlimenticioRepository;
+import uca.ungallocontenis.kenkougymapi.repository.Nutricion.RecetaRepository;
+import uca.ungallocontenis.kenkougymapi.repository.Nutricion.TiempoComidaRepository;
 
 @RestController
 @RequestMapping("/nutricion")
 public class NutricionController {
-    
+
+    @Autowired
+    private RecetaRepository recetaRepository;
+    @Autowired
+    private TiempoComidaRepository tiempoRepository;
+    @Autowired
+    private PlanAlimenticioRepository planRepository;
+    @Autowired
+    private IngredienteRepository ingredienteRepository;
+    @Autowired
+    private EstiloAlimenticioRepository estiloRepository;
+
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(NutricionController.class);
 }
